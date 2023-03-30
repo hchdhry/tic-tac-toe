2f9309a4aa88e8e10dcbf34cd5dd2fad13c15e5e
hassan <hchoudhury20@hotmail.com>
2023-03-30 19:20:31 +0100

    reduce global code via the use of objects and factory functions

--- t-t-t.js
+++ t-t-t.js
@@ -1,16 +1,18 @@
-const game= (()=>{
-   const gameboard=[
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-  ];
+const gameboard={
+    board: Array(9).fill(''),    
+  }
+ 
+  const player=(symbol,value)=>{
+    function populate(player,index){
+      gameboard.board[index]= player
 
-function mark(){
+    }
 
-}
-return {gameboard}
-
-})();
-console.log(game.gameboard[2][1])
+    return{symbol,value,populate}
+  }
 
   
+  const playerone = player("x",1)
+  const playertwo = player("o",2)
+ playerone.populate("x",2)
+  console.log(gameboard.board)
